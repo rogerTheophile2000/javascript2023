745ebd72c182d260313d7153df43b333771e90c4
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-04-29 12:24:54 +0300

    bro code course first commit

---
+++ bro code/index.js
@@ -0,0 +1,54 @@
+// let age = 23;
+// let firstname = "Roger";
+// let student = true;
+
+// age = age + 1 
+
+
+
+// console.log(age);
+// console.log(firstname);
+// console.log(student);
+
+// console.log("Your are " + age + " years old");
+// console.log("Your are", age, "years old");
+
+// document.getElementById("p1").innerHTML = "this course is interesting"
+// document.getElementById("p1").innerHTML = "Hello " + firstname + " are you OK"
+
+/************************************** */
+// Templates literals
+/************************************** */
+
+// let username = "Roger";
+// let item = 3;
+// let total =100; 
+
+// let text = `
+// Hello ${username}  <br>
+// you have ${item} items cart. <br>
+// Your total is ${total}. <br>
+// `;
+// console.log(text);
+// document.getElementById("myPar").innerHTML = text
+
+
+/************************************** */
+// toLocaleString
+/************************************** */
+
+// let myNum = 100
+
+// myNum = myNum.toLocaleString("en-US");
+// myNum = myNum.toLocaleString("hi-IN");
+// myNum = myNum.toLocaleString("de-DE");
+// myNum = myNum.toLocaleString("fr-FR");
+
+// myNum = myNum.toLocaleString("fr-FR", {style:"currency", currency:"EUR"});
+// myNum = myNum.toLocaleString("en-US", {style:"currency", currency:"USD"});
+// myNum = myNum.toLocaleString("hi-IN", {style:"currency", currency:"INR"});
+
+// myNum = myNum.toLocaleString(undefined, {style:"percent"});
+// myNum = myNum.toLocaleString(undefined, {style:"unit", unit:"celsius"});
+
+// console.log(myNum);
