c34c1a370bd514caec2475cb3a640306f61204c2
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-05 06:48:31 +0300

    begining of rock-papper-scissor game

---
+++ piere-papaier-ciseau/main.js
@@ -0,0 +1,11 @@
+const playerText = document.querySelector("#player-text");
+const computerText = document.querySelector("#computer-text");
+const choiceText = document.querySelector("#result-tex");
+
+const choiceBtns = document.querySelectorAll(".choice-btn");
+
+let player;
+let computer;
+let result;
+
+choiceBtns
