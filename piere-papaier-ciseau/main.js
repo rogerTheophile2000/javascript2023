f901d06528e65c9b2a0ce353211d84341512012e
Roger Mushagalusa <rogetheophile2000@gmail.com>
2023-05-05 07:00:54 +0300

    finish html and js part of paper-scissor-rock game

--- piere-papaier-ciseau/main.js
+++ piere-papaier-ciseau/main.js
@@ -1,5 +1,6 @@
 const playerText = document.querySelector("#player-text");
 const computerText = document.querySelector("#computer-text");
+const resultText = document.querySelector("#result-text");
 const choiceText = document.querySelector("#result-tex");
 
 const choiceBtns = document.querySelectorAll(".choice-btn");
@@ -8,4 +9,42 @@ let player;
 let computer;
 let result;
 
-choiceBtns
+choiceBtns.forEach((button) =>
+  button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = `Player : ${player}`;
+    computerText.textContent = `Computer : ${computer}`;
+    resultText.textContent = checkWinner();
+  })
+);
+
+function computerTurn() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (randNum) {
+    case 1:
+      computer = "ROCK";
+      break;
+
+    case 2:
+      computer = "PAPER";
+      break;
+
+    case 3:
+      computer = "SCISSORS";
+      break;
+  }
+}
+
+function checkWinner() {
+  if (player == computer) {
+    return "Draw !";
+  } else if (computer == "ROCK") {
+    return player == "PAPER" ? "You win !" : "You Loose !";
+  } else if (computer == "PAPER") {
+    return player == "SCISSORS" ? "You win !" : "You Loose !";
+  } else if (computer == "SCISSORS") {
+    return player == "ROCK" ? "You win !" : "You Loose !";
+  }
+}
